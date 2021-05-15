5311fea819b587633cf44ea750948504dfb0e9fc
Björn Kökeritz <bjorn.kokeritz@gmail.com>
2021-05-15 23:13:32 +0200

    Done

--- CoolStuff.js
+++ CoolStuff.js
@@ -1,17 +1,5 @@
-var slideIndex = 0;
-showSlides();
 
-function showSlides() {
-  var slides = document.getElementsByClassName("frontpage");
-  if (slideIndex == 0){slides[0].style.cssText = "background-image: url(img/Coffee1.jpg);"}
-  if (slideIndex == 1){slides[0].style.cssText = "background-image: url(img/Coffee1.jpg);"}
-  if (slideIndex == 2){slides[0].style.cssText = "background-image: url(img/Coffee2.jpg);"}
-  if (slideIndex == 3){slides[0].style.cssText = "background-image: url(img/Coffee3.jpg);"}
-  if (slideIndex == 4){slides[0].style.cssText = "background-image: url(img/Coffee4.jpg);"}
-  slideIndex++;
-  if (slideIndex > 4) {slideIndex = 1}
-  setTimeout(showSlides, 4000); 
-}
+
 function openNav() {
   document.getElementById("mySidenav").style.width = "300px";
 }
